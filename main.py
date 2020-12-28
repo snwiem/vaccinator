099a7efd94442820a6989479ad809b8086cbf37b
snwiem <snwiem@googlemail.com>
2020-12-28 20:17:27 +0100

    screen

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import os, sys
+import pygame
+from pygame.locals import *
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+if not pygame.font:
+    print('Warning, fonts disabled')
+if not pygame.mixer:
+    print('Warning, sound disabled')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error as message:
+        print('Cannot load sound:', fullname)
+        raise SystemExit(message)
+    return sound
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+screen = pygame.display.set_mode((600, 480))
+pygame.display.set_caption("PyHunt")
+pygame.mouse.set_visible(True)
+
+clock = pygame.time.Clock()
+loop = True
+while loop:
+    for event in pygame.event.get():
+        print("Event: ", event)
+        if event.type == QUIT:
+            loop = False
+    clock.tick(60)
+
+print("All done...bye")
+
